ff2551297ffe2d2272cd9c4d8a7d6724c612ae89
harland-jensen <harlandctj@gmail.com>
2022-09-26 10:39:01 +1000

    Added functionality to randomly generate location of marker

--- villers_boys_ii/lib/reaction_time_test1.dart
+++ villers_boys_ii/lib/reaction_time_test1.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:villers_boys_ii/reaction_time_intro.dart';
 import 'package:villers_boys_ii/user.dart';
+import 'dart:math';
+import 'dart:async';
+
 
 import 'memory_test_intro.dart';
 
@@ -32,12 +35,14 @@ class ReactionTestState extends State<ReactionTimeTest> {
     0,
   ];
 
+
+
   @override
   Widget build(BuildContext context) {
     if (true) {
       setState(() {
         Widget build(BuildContext context) {
-          return MaterialApp(home: new ReactionTimeIntro(user: user));
+          return MaterialApp(home: ReactionTimeIntro(user: user));
         }
       });
     }
@@ -51,84 +56,35 @@ class ReactionTestState extends State<ReactionTimeTest> {
           textAlign: TextAlign.center,
         )),
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => MemoryTestIntro(user: widget.user)));
-              },
-              child: Icon(
-                Icons.start_sharp,
-                size: 50,
-              ),
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  height: HEIGHT,
-                  width: WIDTH,
-                  child: Align(
-                    alignment: Alignment.topLeft,
+              child: Stack(children: [
+                Positioned(
+                  left: Random().nextDouble() * (MediaQuery.of(context).size.width - 100 - (100)) + 100,
+                  //top: Random().nextDouble() * (MediaQuery.of(context).size.height / 2),
+                  //right: Random().nextDouble() * (MediaQuery.of(context).size.width / 2),
+                  bottom: Random().nextDouble() * ((MediaQuery.of(context).size.height - 100) - (100)) + 100,
+
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
                     child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Top Left"),
-                      style: ElevatedButton.styleFrom(
-                        //backgroundColor: Colors.lightBlueAccent,
-                        fixedSize: Size(WIDTH, HEIGHT),
-                      ),
+
+                      onPressed: (){}, child: const Icon(
+                      Icons.close_rounded,
                     ),
-                  ),
+                    ),
+                  )
                 ),
-                SizedBox(
-                  height: HEIGHT,
-                  width: WIDTH,
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Top Right"),
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: Size(WIDTH, HEIGHT)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                SizedBox(
-                    height: HEIGHT,
-                    width: WIDTH,
-                    child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              fixedSize: Size(WIDTH, HEIGHT)),
-                          onPressed: () {},
-                          child: Text(
-                            "Bottom Left",
-                          ),
-                        ))),
-                SizedBox(
-                  height: HEIGHT,
-                  width: WIDTH,
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Bottom Right"),
-                      style: ElevatedButton.styleFrom(
-                          //backgroundColor: Colors.lightBlueAccent,
-                          fixedSize: Size(WIDTH, HEIGHT)),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
-        )));
+
+              ],)
+
+        ));
   }
 }
+//Navigator.of(context).push(MaterialPageRoute(
+//   builder: (context) => MemoryTestIntro(user: widget.user)));
+
+// Timer(Duration(milliseconds: 1000), () {
+// setState(() {
+//
+// });
+// })
