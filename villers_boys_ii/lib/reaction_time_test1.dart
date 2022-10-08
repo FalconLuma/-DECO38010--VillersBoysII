1ae43840b23db5e775a0b410355495b2cb5c0426
kudzi <kudzi.maphosa@gmail.com>
2022-10-08 17:11:07 +1000

    Fixed reaction time button and icon position. Avoided navigating from inside setState(). Clean up & documentation

--- villers_boys_ii/lib/reaction_time_test1.dart
+++ villers_boys_ii/lib/reaction_time_test1.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:villers_boys_ii/drive_assessment.dart';
-import 'package:villers_boys_ii/reaction_time_intro.dart';
 import 'package:villers_boys_ii/user.dart';
 import 'package:collection/collection.dart';
 import 'dart:math';
@@ -22,72 +21,41 @@ class ReactionTimeTest extends StatefulWidget {
   DriveAssessment? driveAssessment;
 
   @override
-  State<ReactionTimeTest> createState() => ReactionTestState(user);
+  State<ReactionTimeTest> createState() => _ReactionTestState();
 }
 
-class ReactionTestState extends State<ReactionTimeTest> {
+class _ReactionTestState extends State<ReactionTimeTest> {
   late SharedPreferences prefs;
-  ReactionTestState(this.user, {Key? key});
-  final User user;
   bool shownIntro = false;
-  int _tnum = 0; // Test number
+  int _tNum = 0; // Test number
   bool calibrate = false;
 
   // Create a list of reaction times
-  List<int> rTimes = [
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-  ];
+  List<int> rTimes = List.filled(10, 0, growable: true);
 
   @override
   Widget build(BuildContext context) {
     int startTime = DateTime.now().millisecondsSinceEpoch;
-    if (true) {
-      setState(() {
-        Widget build(BuildContext context) {
-          return MaterialApp(
-              home: ReactionTimeIntro(
-            user: user,
-            calibrate: widget.calibrate,
-            driveAssessment: widget.driveAssessment,
-          ));
-        }
-      });
-    }
-    double WIDTH = (MediaQuery.of(context).size.width) / 2;
-    double HEIGHT = (MediaQuery.of(context).size.height) / 6;
-    double left =
-        Random().nextDouble() * (MediaQuery.of(context).size.width - 100);
-    double top =
-        Random().nextDouble() * (MediaQuery.of(context).size.height - 100);
+    double buttonWidth = 50;
 
-    //Check to make sure values aren't less than 150 i.e. button can appear off-screen
-    // if (left < 150) {
-    //   left = 150;
-    // }
-    if (top < 0) {
-      top = 0;
-    }
-    //Check to make sure values aren't within 150 pixels of edge of screen
-    // if (left > (MediaQuery.of(context).size.width - 150)) {
-    //   left = MediaQuery.of(context).size.width - 150;
-    // }
-    if (top > (MediaQuery.of(context).size.height)) {
-      top = MediaQuery.of(context).size.height;
-    }
+    // Screen size. Account for status bar, app bar, and button width for height.
+    double width = MediaQuery.of(context).size.width - buttonWidth;
+    double height = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).padding.top - kToolbarHeight - buttonWidth;
+
+    // Margins for button locations.
+    double margin = 0.05;
+
+    // Button locations.
+    double left = margin * width +
+        Random().nextDouble() * (width - 2 * margin * width);
+    double top = margin * height +
+        Random().nextDouble() * (height - 2 * margin * height);
 
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            "Test: ${_tnum + 1}/10 ",
+            "Test: ${_tNum + 1}/10 ",
             style: const TextStyle(fontSize: 25),
             textAlign: TextAlign.center,
           ),
@@ -129,88 +97,71 @@ class ReactionTestState extends State<ReactionTimeTest> {
             },
           ),
         ),
-        body: Center(
-            child: Stack(
-          children: [
-            Positioned(
-                left: left,
-                top: top,
-                child: SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      int endTime = DateTime.now().millisecondsSinceEpoch;
-                      rTimes[_tnum] = endTime - startTime;
-                      setState(() {
-                        if (_tnum < 9) {
-                          _tnum++;
-                        } else {
-                          //Disregard two smallest reaction time measurements
-                          rTimes.removeAt(getMin(rTimes));
-                          rTimes.removeAt(getMin(rTimes));
-                          //Disregard two largest reaction time measurements
-                          rTimes.removeAt(getMax(rTimes));
-                          rTimes.removeAt(getMax(rTimes));
-                          //Calculate average
-                          double average = rTimes.average;
-                          debugPrint(rTimes.toString());
-                          debugPrint("Your average reaction time is: $average");
-                          //Put this in an if statment to only run if its a setup/calibration version
-                          if (widget.calibrate == false) {
-                            save(average);
+        body: Stack(
+            children: <Widget>[
+              Positioned(
+                  left: left,
+                  top: top,
+                  child: SizedBox(
+                      width: buttonWidth,
+                      height: buttonWidth,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero
+                        ),
+                        onPressed: () {
+                          int endTime = DateTime.now().millisecondsSinceEpoch;
+                          rTimes[_tNum] = endTime - startTime;
+                          if (_tNum < 9) {
+                            // Set the state to new button location for each test.
+                            setState(() {
+                              _tNum++;
+                            });
+                            debugPrint(rTimes.toString());
                           } else {
-                            widget.driveAssessment?.setReactionTime(average);
-                          }
-                          Navigator.of(context).push(MaterialPageRoute(
+                            //Disregard two smallest reaction time measurements
+                            rTimes.remove(rTimes.min);
+                            rTimes.remove(rTimes.min);
+
+                            //Disregard two largest reaction time measurements
+                            rTimes.remove(rTimes.max);
+                            rTimes.remove(rTimes.max);
+
+                            //Calculate average
+                            double average = rTimes.average;
+                            debugPrint(rTimes.toString());
+                            debugPrint("Your average reaction time is: $average");
+
+                            //Put this in an if statement to only run if its a setup/calibration version
+                            if (widget.calibrate == false) {
+                              save(average);
+                            } else {
+                              widget.driveAssessment?.setReactionTime(average);
+                            }
+
+                            Navigator.of(context).push(MaterialPageRoute(
                               builder: (context) => MemoryTestIntro(
-                                    user: widget.user,
-                                    calibrate: widget.calibrate,
-                                    driveAssessment: widget.driveAssessment,
-                                  ),
-                              settings: RouteSettings(name: ModalRoute.of(context)!.settings.name)));
-                        }
-                      });
-                    },
-                    child: const Icon(
-                      Icons.close_rounded,
-                    ),
-                  ),
-                )),
-          ],
-        )));
+                                user: widget.user,
+                                calibrate: widget.calibrate,
+                                driveAssessment: widget.driveAssessment,
+                              ),
+                                settings: RouteSettings(name: ModalRoute.of(context)!.settings.name)
+                            ));
+                          }
+                          },
+                        child: const Icon(Icons.close_rounded),
+                      )
+                  )
+              )
+            ]
+        )
+    );
   }
 
+  /// Save the user's baseline reaction test time to the device.
   save(average) async {
     prefs = await SharedPreferences.getInstance();
     //prefs.setDouble("reaction", 0.0);
     prefs.setDouble("reactionTemp", average);
   }
-}
-//Navigator.of(context).push(MaterialPageRoute(
-//   builder: (context) => MemoryTestIntro(user: widget.user)));
-
-// Timer(Duration(milliseconds: 1000), () {
-// setState(() {
-//
-// });
-// })
-getMin(List rTimes) {
-  int minIndex = 0;
-  for (int i = 0; i < rTimes.length; i++) {
-    if (rTimes[i] <= rTimes[minIndex]) {
-      minIndex = i;
-    }
-  }
-  return minIndex;
-}
-
-getMax(List rTimes) {
-  int maxIndex = 0;
-  for (int i = 0; i < rTimes.length; i++) {
-    if (rTimes[i] >= rTimes[maxIndex]) {
-      maxIndex = i;
-    }
-  }
-  return maxIndex;
 }
